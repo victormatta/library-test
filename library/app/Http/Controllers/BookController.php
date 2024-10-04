1343cbf8aa2c2bd67de93991260b528493462f69
Victor Matta <vmatta0708@gmail.com>
2024-10-03 23:11:28 -0300

    create project controllers

---
+++ library/app/Http/Controllers/BookController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController  extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return view("books.index", compact("books"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'isbn' => 'required|unique:books'
+        ]);
+
+        Book::create($request->all());
+        return redirect()->route('books.index')->with('success', 'Book added successfully!');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index')->with('success','Book removed successfully!');
+    }
+}
---
+++ library/app/Http/Controllers/LoanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Loan;
+use App\Models\User;
+use App\Models\Book;
+
+class LoanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $loans = Loan::with(['book', 'user'])->get();
+        return view('loans.index', compact('loans'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $bookd = Book::all();
+        $users = User::all();
+        return view('loans.create', compact('books', 'users'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'book_id' => 'required|exists:books,id',
+            'date_loan' => 'required|date',
+        ]);
+
+        Loan::create($request->all());
+        return redirect()->route('loans.index')->with('success','Loan done successfully!');
+        
+    }
+
+    public function back(Loan $loan) {
+        $loan->date_return = now();
+        $loan->save();
+
+        return redirect()->route('loans.index')->with('success','Book returned successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
