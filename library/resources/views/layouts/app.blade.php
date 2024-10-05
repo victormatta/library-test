d7edf23b1827be3d519f2ed894a330a8c8a04810
Victor Matta <vmatta0708@gmail.com>
2024-10-05 16:21:53 -0300

    Add toggle button

--- library/resources/views/layouts/app.blade.php
+++ library/resources/views/layouts/app.blade.php
@@ -18,12 +18,14 @@
             background-color: #007bff;
             color: white;
         }
-            </style>
-    
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <a class="navbar-brand text-light" href="{{route('static.initPage')}}"><i class="bi bi-book"></i></a>
+        <a class="navbar-brand text-light" href="{{ route('static.initPage') }}"><i class="bi bi-book"></i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -38,8 +40,13 @@
             </ul>
         </div>
     </nav>
+
     <div class="container mt-4">
         @yield('content')
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
